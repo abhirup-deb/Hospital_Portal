54df2072a07c5dda09107be85cab4d666a633a0c
abhirup-deb <debabhirup1999@gmail.com>
2020-02-15 21:43:13 +0530

    Access Permission added

--- lib/screens/Registration.dart
+++ lib/screens/Registration.dart
@@ -134,22 +134,22 @@ class _RegistrationState extends  State<Registration>{
                     }
                   });
                   if(_validate1==false && _validate2==false && _validate3==false && digit==true){
-                    setState(()async {
-                      var databasesPath = await getDatabasesPath();
-                      String path = p.join(databasesPath, 'demo.db');
-                      var db = await openDatabase(path);
-
-
-                      await db.transaction((txn) async {
-                        int id1 = await txn.rawInsert('INSERT (Name, Aadhar, Email, Contact, Address) VALUES(_name, _aadhar, _email, _num, _add)');
-                        print(id1);
-                      });
-                      await db.close();
-                    });
+//                    setState(()async {
+//                      var databasesPath = await getDatabasesPath();
+//                      String path = p.join(databasesPath, 'demo.db');
+//                      var db = await openDatabase(path);
+//
+//
+//                      await db.transaction((txn) async {
+//                        int id1 = await txn.rawInsert('INSERT (Name, Aadhar, Email, Contact, Address) VALUES(_name, _aadhar, _email, _num, _add)');
+//                        print(id1);
+//                      });
+//                      await db.close();
+//                    });
 
 
                     Flushbar(
-                      message: "Registered Successfully",
+                      message: "Certificate will be Generated once Verified",
                       icon: Icon(
                         Icons.event_available,
                         size: 20.0,
